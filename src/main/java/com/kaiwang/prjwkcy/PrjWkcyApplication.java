84514c67e935238b4366244fcfb1c21f1d666c53
493036022@qq.com <493036022@qq.com>
2021-06-26 21:05:10 +0800

    配置完成。项目可以运行

--- src/main/java/com/kaiwang/prjwkcy/PrjWkcyApplication.java
+++ src/main/java/com/kaiwang/prjwkcy/PrjWkcyApplication.java
@@ -1,6 +1,7 @@
 package com.kaiwang.prjwkcy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import xyz.erupt.annotation.Erupt;
